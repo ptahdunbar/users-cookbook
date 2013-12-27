1bcc85da9137734bb1597aef6388a4ce4c52410c
Pirate Dunbar <yarr@piratedunbar.com>
2013-12-27 04:06:50 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'yarr@piratedunbar.com'
 license          'All rights reserved'
 description      'Installs/Configures users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.9'
 
